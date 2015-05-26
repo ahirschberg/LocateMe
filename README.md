8399dbcd535061c223f5ae64e0bbbd4b75384b9a
ahirschberg <googlestinks@ymail.com>
2015-05-26 15:38:28 -0400

    Updated git paths to match project directory

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 LocateMe App
 ============
 
-Firefox OS App for displaying current location on a map.
+Firefox OS App for displaying a user's current location on a map.
 
-This project was updated from the version Episode 5 of the Firefox
-OS Tutorial available at
-[http://rominirani.com/2013/07/31/firefox-os-tutorial-episode-5-location-location-location/](http://rominirani.com/2013/07/31/firefox-os-tutorial-episode-5-location-location-location/)
-
-It has been updated and modified.
+This app was written from scratch by Alex Hirschberg.  The icons are pulled from Chapter 5 of Roman Irani's Firefox OS tutorial series.
 
 The tutorial that discusses the app can be found at
 [http://www.openbookproject.net/tutorials/fxos/](http://www.openbookproject.net/tutorials/fxos/)
--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-$(document).ready(function() {
-    $("#btnLocateMe").click(function(){
-        findMyCurrentLocation();
+function leafletFindLocation () {
+    var map = L.map('map');
+
+    // toolserver.org hosts OpenStreetMap tile data
+    L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+      attribution: 'Map data © OpenStreetMap contributors',
+      maxZoom: 18
+    }).addTo(map);
+    
+    // setup location events
+    map.on('locationfound', function (event) {
+        var radius = event.accuracy / 2;
+
+        L.marker(event.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(event.latlng, radius).addTo(map);
+    });
+    map.on('locationerror', function () {
+        alert(e.message);
+    });
+    
+    map.locate({setView: true, maxZoom: 16});
+}
+
+$(document).ready(function () {
+    $("#locateMe").click(function() {
+        leafletFindLocation();
+        $(this).hide();
     });
 });
-
-function findMyCurrentLocation() {
-    var geoService = navigator.geolocation;
-    if (geoService) {
-        navigator.geolocation.getCurrentPosition(showCurrentLocation,
-                                                 errorHandler);
-    } else {
-        $("#searchResults").html("Your Browser does not support GeoLocation.");
-    }
-}
-
-function showCurrentLocation(position) {
-    $("#searchResults").html("Your location details --> Current Latitude : " + position.coords.latitude + " , Longitude : " + position.coords.longitude);
-}
-
-function errorHandler(error) {
-    $("#searchResults").html("Error while retrieving current position. Error code: " + error.code + ",Message: " + error.message);
-}
